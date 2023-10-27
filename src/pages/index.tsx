872ccdbfc4b2fc337f5b54c0eb2858fca795fd40
nogi <Createc369.Re@gmail.com>
2023-10-27 17:55:52 +0900

    add: routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
 export default function TodoListPage() {
-  return <h1>Hello</h1>;
+  return <h1>Index</h1>;
 }
+
+// http://localhost:3001/
+// http://localhost:3001/todo/create
+// http://localhost:3001/todo/detail/1
+// http://localhost:3001/todo/edit/1
